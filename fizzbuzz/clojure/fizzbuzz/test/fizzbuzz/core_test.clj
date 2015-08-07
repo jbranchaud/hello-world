ee8395b757f7f30d2967eff1699a542dd4e72fb0
jbranchaud <jbranchaud@gmail.com>
2015-08-07 15:28:08 -0500

    Update the clojure fizzbuzz tests.

--- fizzbuzz/clojure/fizzbuzz/test/fizzbuzz/core_test.clj
+++ fizzbuzz/clojure/fizzbuzz/test/fizzbuzz/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [fizzbuzz.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest fizzbuzz-val-test
+  (testing "Returns fizz for multiple of 3"
+    (is (= "fizz" (fizzbuzz-val 9))))
+  (testing "Returns buzz for multiple of 5"
+    (is (= "buzz" (fizzbuzz-val 10))))
+  (testing "Returns fizzbuzz for multiple of 15"
+    (is (= "fizzbuzz" (fizzbuzz-val 15))))
+  (testing "Returns stringified number for other values")
+    (is (= "13" (fizzbuzz-val 13))))
+
+(deftest generic-fizzbuzz-test
+  (testing "Returns vector of fizzbuzz results"
+    (is (= ["1" "2" "fizz" "4" "buzz" "fizz" "7" "8" "fizz"]
+           (generic-fizzbuzz 9)))))
+
+(deftest fizzbuzz-test
+  (testing "Returns vector of 100 fizzbuzz results"
+    (is (= 100 (count (fizzbuzz))))))
