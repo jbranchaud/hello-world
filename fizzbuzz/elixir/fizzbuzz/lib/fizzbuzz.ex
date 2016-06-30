7098b809a3d7d31095d55ad93b98d9832a4676c6
jbranchaud <jbranchaud@gmail.com>
2016-06-30 16:14:39 -0500

    Use private func declarations for internal funcs

--- fizzbuzz/elixir/fizzbuzz/lib/fizzbuzz.ex
+++ fizzbuzz/elixir/fizzbuzz/lib/fizzbuzz.ex
@@ -6,20 +6,20 @@ defmodule Fizzbuzz do
   end
 
   def fizzbuzz, do: fizzbuzz(1)
-  def fizzbuzz(101), do: nil
-  def fizzbuzz(n) when rem(n, 15) == 0 do
+  defp fizzbuzz(101), do: nil
+  defp fizzbuzz(n) when rem(n, 15) == 0 do
     IO.puts "FizzBuzz"
     fizzbuzz(n + 1)
   end
-  def fizzbuzz(n) when rem(n, 5) == 0 do
+  defp fizzbuzz(n) when rem(n, 5) == 0 do
     IO.puts "Buzz"
     fizzbuzz(n + 1)
   end
-  def fizzbuzz(n) when rem(n, 3) == 0 do
+  defp fizzbuzz(n) when rem(n, 3) == 0 do
     IO.puts "Fizz"
     fizzbuzz(n + 1)
   end
-  def fizzbuzz(n) do
+  defp fizzbuzz(n) do
     IO.puts n
     fizzbuzz(n + 1)
   end
